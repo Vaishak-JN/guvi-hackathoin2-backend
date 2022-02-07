545c8d74585bdde34b6391a6aa424ecf42b1290a
Vaishak-JN <vaishakjnair.98@gmail.com>
2022-02-07 16:55:30 +0530

    cors

--- index.js
+++ index.js
@@ -11,32 +11,9 @@ dotenv.config();
 
 const app = express()
 
-app.use(express.json())
 
-const whitelist = ["https://vrentalapp.herokuapp.com"]
+app.use(cors())
 
-const corsOptions = {
-
-    origin: function (origin, callback) {
-
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-
-            callback(null, true)
-
-        } else {
-
-            callback(new Error("Not allowed by CORS"))
-
-        }
-
-    },
-
-    credentials: true,
-
-}
-
-// app.use(cors())
-app.use(cors(corsOptions))
 
 
 // const MONGO_URL = "mongodb://localhost"
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import { createUser, genPassword, getUserByName } from "../productFunctions.js";
 
+
 const router = express.Router()
 
 router
